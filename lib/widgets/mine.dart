34a0d7f3ef5412b23a260dd012ed261ffe80931c
zpz5789 <1059491105@qq.com>
2020-02-14 22:15:24 +0800

    first commit

---
+++ lib/widgets/mine.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+
+class MinePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text('我的页面'),
+        )
+      ),
+    );
+  }
+
+  Widget _mineHeader() {
+    return Container(
+
+    );
+  }
+
+
+}
+
+
